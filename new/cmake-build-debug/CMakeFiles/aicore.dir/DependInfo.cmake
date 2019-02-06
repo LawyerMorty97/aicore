b1d258a065c50d887cab393c3061e33023161a47
Mathias Berntsen <mathiastb13@gmail.com>
2019-02-07 00:46:06 +0100

    Progress

---
+++ new/cmake-build-debug/CMakeFiles/aicore.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/aicore/new/action.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/action.cpp.obj"
+  "G:/aicore/new/aimath.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/aimath.cpp.obj"
+  "G:/aicore/new/basesm.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/basesm.cpp.obj"
+  "G:/aicore/new/core.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/core.cpp.obj"
+  "G:/aicore/new/dectree.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/dectree.cpp.obj"
+  "G:/aicore/new/kinematic.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/kinematic.cpp.obj"
+  "G:/aicore/new/learning.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/learning.cpp.obj"
+  "G:/aicore/new/location.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/location.cpp.obj"
+  "G:/aicore/new/markovsm.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/markovsm.cpp.obj"
+  "G:/aicore/new/qlearning.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/qlearning.cpp.obj"
+  "G:/aicore/new/rules.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/rules.cpp.obj"
+  "G:/aicore/new/sm.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/sm.cpp.obj"
+  "G:/aicore/new/steering.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/steering.cpp.obj"
+  "G:/aicore/new/steerpipe.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/steerpipe.cpp.obj"
+  "G:/aicore/new/timing.cpp" "G:/aicore/new/cmake-build-debug/CMakeFiles/aicore.dir/timing.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ new/cmake-build-debug/CMakeFiles/aicore.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aicore.dir/action.cpp.obj"
+  "CMakeFiles/aicore.dir/aimath.cpp.obj"
+  "CMakeFiles/aicore.dir/basesm.cpp.obj"
+  "CMakeFiles/aicore.dir/core.cpp.obj"
+  "CMakeFiles/aicore.dir/dectree.cpp.obj"
+  "CMakeFiles/aicore.dir/kinematic.cpp.obj"
+  "CMakeFiles/aicore.dir/learning.cpp.obj"
+  "CMakeFiles/aicore.dir/location.cpp.obj"
+  "CMakeFiles/aicore.dir/markovsm.cpp.obj"
+  "CMakeFiles/aicore.dir/qlearning.cpp.obj"
+  "CMakeFiles/aicore.dir/rules.cpp.obj"
+  "CMakeFiles/aicore.dir/sm.cpp.obj"
+  "CMakeFiles/aicore.dir/steering.cpp.obj"
+  "CMakeFiles/aicore.dir/steerpipe.cpp.obj"
+  "CMakeFiles/aicore.dir/timing.cpp.obj"
+  "libaicore.pdb"
+  "libaicore.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aicore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
